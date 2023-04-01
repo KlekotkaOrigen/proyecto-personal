a3873ae55b03b75c2bebec444e3f79d224eff1e4
KlekotkaOrigen <fudomyosama@hotmail.com>
2023-04-01 19:35:27 -0300

    Add Product List

--- src/components/pages/Listado/index.jsx
+++ src/components/pages/Listado/index.jsx
@@ -1,3 +1,29 @@
-const Listado = () => ("Esto es el LIstado");
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import * as csv from "csvtojson";
+
+const PRODUCTS_URL =
+"https://docs.google.com/spreadsheets/d/e/2PACX-1vQnrtnTqpzr-izreIsykOv1uIcxUWJ7ic295jClos-bjpJwK-LfORpGQCALOiZs2e1lUJKFaVtPU-c7/pub?output=csv";
+const Listado = () => {
+const [productos, setProductos] = useState([]);
+useEffect(() => {
+    fetch(PRODUCTS_URL)
+    .then((response) => response.text())
+    .then((data) => csv().fromString(data))
+    .then((objetos) => {
+        objetos.forEach(
+        (objeto) => (objeto.categoria = objeto.categoria.split(","))
+        );
+        setProductos(objetos);
+    });
+}, []);
+const { categoria } = useParams();
+
+  // return productos.map(producto => producto.nombre)
+return (productos
+    .filter((producto) => producto.categoria.includes(categoria))
+    .map((producto) => <img key={producto.id} src={producto.photo} />)
+    )
+};
 
 export default Listado;
