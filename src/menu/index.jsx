4e4e1b64c1299d6f22f359b64f124502fda07638
KlekotkaOrigen <fudomyosama@hotmail.com>
2023-04-02 20:32:53 -0300

    Closes Menu Productos

--- src/menu/index.jsx
+++ src/menu/index.jsx
@@ -5,46 +5,66 @@ import { context } from "../components/contexts/DolarContext";
 import ReactModal from "react-modal";
 import { Link } from "react-router-dom";
 
-const Menu = props => {
+const Menu = (props) => {
   const [isPricesOpen, setPricesOpen] = useState(false);
   const [isProductsOpen, setProductsOpen] = useState(false);
-  const valor = useContext (context)
-  console.log("VALOR DEL CONTEXTO", valor)
+  const valor = useContext(context);
+  console.log("VALOR DEL CONTEXTO", valor);
 
   return (
     <div className={styles.main}>
-        <div className={styles.menuButton}>
-          <Button onClick={() => setProductsOpen(!isProductsOpen)}>PRODUCTOS</Button>
-          <ul className={isProductsOpen ? styles.productsOpen : styles.productsClosed}>
-            <li className={styles.productos}><Link to="computacion">COMPUTACIÓN</Link></li>
-            <li className={styles.productos}><Link to="repuestoscomp">REPUESTOS COMPUTACION</Link></li>
-            <li className={styles.productos}><Link to="acccelulares">ACCESORIOS CELULARES</Link></li>
-            <li className={styles.productos}><Link to="repuestoscel">REPUESTOS CELULARES</Link></li>
-            <li className={styles.productos}><Link to="usados">ARTÍCULOS USADOS</Link></li>
-            <li className={styles.productos}><Link to="propios">PRODUCTOS PROPIOS</Link></li>
-            <li className={styles.productos}><Link to="outlet">OUTLET</Link></li>
-          </ul>
-        </div>
-        <div>
-          <Button onClick={() => setPricesOpen(true)}>FORMAS DE PAGO</Button>
-        </div>
-        <ReactModal
-          overlayClassName={styles.modalOverlay}
-          className={styles.modal}
-          isOpen={isPricesOpen}
-          onRequestClose={() => setPricesOpen(false)}
+      <div className={styles.menuButton}>
+        <Button onClick={() => setProductsOpen(!isProductsOpen)}>
+          PRODUCTOS
+        </Button>
+        <ul
+          className={
+            isProductsOpen ? styles.productsOpen : styles.productsClosed
+          }
         >
-          <img
-            src="./pago.jpg"
-            alt="formas de pago tarjetar de credito y debito, transferencia bancarias"
-          />
-          <button className={styles.close} onClick={() => setPricesOpen(false)}>
-            <img src="./close.png" alt="Boton para cerrar" />
-          </button>
-        </ReactModal>
-        <button className={styles.dolar}>U$D: {valor}</button>
+          <li className={styles.productos} onClick={styles.productsClosed}>
+            <Link to="computacion">COMPUTACIÓN</Link>
+          </li>
+          <li className={styles.productos} onClick={styles.productsClosed}>
+            <Link to="repuestoscomp">REPUESTOS COMPUTACION</Link>
+          </li>
+          <li className={styles.productos} onClick={styles.productsClosed}>
+            <Link to="acccelulares">ACCESORIOS CELULARES</Link>
+          </li>
+          <li className={styles.productos} onClick={styles.productsClosed}>
+            <Link to="repuestoscel">REPUESTOS CELULARES</Link>
+          </li>
+          <li className={styles.productos} onClick={styles.productsClosed}>
+            <Link to="usados">ARTÍCULOS USADOS</Link>
+          </li>
+          <li className={styles.productos} onClick={styles.productsClosed}>
+            <Link to="propios">PRODUCTOS PROPIOS</Link>
+          </li>
+          <li className={styles.productos} onClick={styles.productsClosed}>
+            <Link to="outlet">OUTLET</Link>
+          </li>
+        </ul>
       </div>
-  )
-}
+      <div>
+        <Button onClick={() => setPricesOpen(true)}>FORMAS DE PAGO</Button>
+      </div>
+      <ReactModal
+        overlayClassName={styles.modalOverlay}
+        className={styles.modal}
+        isOpen={isPricesOpen}
+        onRequestClose={() => setPricesOpen(false)}
+      >
+        <img
+          src="./pago.jpg"
+          alt="formas de pago tarjetar de credito y debito, transferencia bancarias"
+        />
+        <button className={styles.close} onClick={() => setPricesOpen(false)}>
+          <img src="./close.png" alt="Boton para cerrar" />
+        </button>
+      </ReactModal>
+      <button className={styles.dolar}>U$D: {valor}</button>
+    </div>
+  );
+};
 
 export default Menu;
