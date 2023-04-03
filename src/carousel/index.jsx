f513f553fd98743ba91ceb955717c4ed5d9dc4d9
KlekotkaOrigen <fudomyosama@hotmail.com>
2023-04-03 18:44:45 -0300

    Changes carousel

--- src/carousel/index.jsx
+++ src/carousel/index.jsx
@@ -1,11 +1,19 @@
 import styles from "./styles.module.css";
-import { Carousel as CarouselResponsive } from 'react-responsive-carousel';
+import { Carousel as CarouselResponsive } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Carousel = (props) => {
-        return (
-            <CarouselResponsive className={styles.main} showArrows={true} autoPlay={false} infiniteLoop={true} showThumbs={false} {...props} />
-        );
+return (
+    <CarouselResponsive
+    className={styles.main}
+    showArrows={true}
+    autoPlay={false}
+    infiniteLoop={true}
+    showThumbs={false}
+    showIndicators={false}
+    {...props}
+    />
+);
 };
 
 export default Carousel;
--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -5,27 +5,12 @@ const Home = () => (
     <Carousel>
         <div>
         <img className={styles.carouselImage} src="./1.JPG" />
-        <p className="legend">CAMARA MOTO G3</p>
         </div>
         <div>
         <img className={styles.carouselImage} src="./2.JPG" />
-        <p className="legend">CARCASA TRASERA LG K4</p>
         </div>
         <div>
         <img className={styles.carouselImage} src="./3.JPG" />
-          <p className="legend">CARGADOR DE AUTO KOSMO</p>
-        </div>
-        <div>
-          <img className={styles.carouselImage} src="./4.JPG" />
-          <p className="legend">MESA NOTEBOOK CON COOLER</p>
-        </div>
-        <div>
-          <img className={styles.carouselImage} src="./5.JPG" />
-          <p className="legend">HDMI TO MICRO USB</p>
-        </div>
-        <div>
-          <img className={styles.carouselImage} src="./6.JPG" />
-          <p className="legend">COOLER AMD6</p>
         </div>
       </Carousel>
 )
