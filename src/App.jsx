474d48e50227f46c8c9686759d78f634114a4233
KlekotkaOrigen <fudomyosama@hotmail.com>
2023-04-04 20:29:07 -0300

    Failed attempt to put footer component´s folder

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,38 @@ import Menu from "./Menu";
 import User from "./user";
 import { Link, Outlet } from "react-router-dom";
 // import Logo from "./components/logo"
+// import Pie from "./components/pie"
 
 function App() {
-
   return (
     <div className={styles.main}>
       <header className={styles.header}>
         {/* <Logo /> */}
         <div className={styles.logo}>
           <Link to="/">
-          <img
-            className={styles.logoimg}
-            src="./logo.png"
-            alt="Esto es el logo de KL VENTAS con palmeras y fondo azul"
-          />
+            <img
+              className={styles.logoimg}
+              src="./logo.png"
+              alt="Esto es el logo de KL VENTAS con palmeras y fondo azul"
+            />
           </Link>
         </div>
         <Input className={styles.search} />
-        <User className={styles.headerRight}/>
+        <User className={styles.headerRight} />
       </header>
       <Menu />
       <Outlet />
       <footer>
         <div>LA EMPRESA</div>
-        <div>FACEBOOK INSTAGRAM</div>
+        <div>FACEBOOK</div>
+        <section className={styles.datos}>
+          <div className={styles.dir}>🌏 Adolfo Alsina 1616 - CABA</div>
+          <div className={styles.tel}>📲 15-2608-1306</div>
+          <div className={styles.web}>📬 CONTACTO WEB</div>
+        </section>
+        <div>INSTAGRAM</div>
         <div>GOOGLE MAPS</div>
-        <div className={styles.datos}>
-          <div>🌏 Adolfo Alsina 1616 - CABA</div>
-          <div>📲 15-2608-1306</div>
-          <div>📬 CONTACTO WEB</div>
-        </div>
+        {/* <Pie></Pie> */}
       </footer>
     </div>
   );
---
+++ src/components/pie/index.jsx
@@ -0,0 +1,16 @@
+import styles from "./components/pie/styles.module.css"
+
+const Pie = () => (
+<section>
+    <div>LA EMPRESA</div>
+    <div>FACEBOOK INSTAGRAM</div>
+    <div>GOOGLE MAPS</div>
+    <section className={styles.datos}>
+        <div>🌏 Adolfo Alsina 1616 - CABA</div>
+        <div>📲 15-2608-1306</div>
+        <div>📬 CONTACTO WEB</div>
+    </section>
+</section>
+);
+
+export default Pie;
