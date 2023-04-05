14d2cbfb4c05cbe53a5657ac640ae52f6df94857
KlekotkaOrigen <fudomyosama@hotmail.com>
2023-04-05 18:37:40 -0300

    Changes styles of cuenta & user

--- src/components/user/index.jsx
+++ src/components/user/index.jsx
@@ -1,24 +1,43 @@
 import styles from "./styles.module.css";
 
 const User = () => (
-<section className={styles.headerRight}>
-    <div className={styles.headerRow}>
-    <img
-        className={styles.carro}
-        src="./carro.png"
-        alt="Este es su carro de compras"
-    />
-    <div>Carro de compras</div>
-    </div>
-    <div className={styles.headerRow}>
-    <img
-        className={styles.cuenta}
-        src="./user.png"
-        alt="Este es el icono de su cuenta"
-    />
-    <div>Cuenta de usuario</div>
-    </div>
-</section>
+    <section>
+        <div>
+        <img
+            className={styles.carro}
+            src="./carro.png"
+            alt="Este es su carro de compras"
+        />
+        </div>
+        <div>
+        <img
+            className={styles.cuenta}
+            src="./user.png"
+            alt="Este es el icono de su cuenta"
+        />
+        </div>
+    </section>
 );
+
+// const User = () => (
+// <section>
+//     <div className={styles.headerRow}>
+//     <img
+//         className={styles.carro}
+//         src="./carro.png"
+//         alt="Este es su carro de compras"
+//     />
+//     <div>Carro de compras</div>
+//     </div>
+//     <div className={styles.headerRow}>
+//     <img
+//         className={styles.cuenta}
+//         src="./user.png"
+//         alt="Este es el icono de su cuenta"
+//     />
+//     <div>Cuenta de usuario</div>
+//     </div>
+// </section>
+// );
 
 export default User;
