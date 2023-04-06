d29961278f758ddd1ce7d23176185049cd9a4da4
KlekotkaOrigen <fudomyosama@hotmail.com>
2023-04-06 16:23:21 -0300

    Modifies styles on Empresa´s page

--- src/components/pages/Empresa/index.jsx
+++ src/components/pages/Empresa/index.jsx
@@ -1,7 +1,19 @@
-import styles from "./styles.module.css"
+import styles from "./styles.module.css";
 
 const Empresa = () => (
-    <div>nuestra mision</div>
+  <div className={styles.bloque}>
+    <h1 className={styles.h1}>KL VENTAS Y ENVIOS</h1>
+    <div className={styles.parrafo}>
+      <p>
+      Con una trayectoria de mas de 20 años en la compra venta de insumos,
+      nuestros productos llegan diariamente a todas partes del país. Nuestro
+      compromiso con el cliente es que el o los productos lleguen en tiempo y
+      forma según lo publicado. Además de brindar una garantía que resuelve en
+      nuestros talleres propios cualquier posible inconveniente. Contamos con
+      productos importados, productos nacionales y productos propios.
+      </p>
+    </div>
+  </div>
 );
 
 export default Empresa;
